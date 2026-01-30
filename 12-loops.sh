2d07a634dc84de75136af0781e6bd04bc21e9bc0
msaivenkatasiva <sivasl0597@gmail.com>
2026-01-30 21:17:28 +0530

    variables

---
+++ 12-loops.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+user_id=$(id -u)
+log_folder="/var/log/shell_script"
+log_file="/var/log/shell_script/$0.log"
+if [ $user_id -ne 0 ];then
+echo "please enter with root access"
+exit 1
+fi
+mkdir -p $log_folder
+validate(){
+    if [ $1 -ne 0 ];then
+    echo "$2 is failed" | tee -a $log_file
+    exit 1
+    else 
+    echo "$2 is success" | tee -a $log_file
+}
+
+for $package in $@
+do 
+dnf install $package -y & >>$log_file
+validate $? "$package installed"
+done 
+
