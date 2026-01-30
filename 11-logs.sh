b54ff94078008edb07e474d69a4fa1c9dd79a415
msaivenkatasiva <sivasl0597@gmail.com>
2026-01-30 19:52:10 +0530

    variables

--- 11-logs.sh
+++ 11-logs.sh
@@ -3,12 +3,13 @@ userid=$(id -u)
 logs_folder="/var/log/shell_script/"
 logs_file="/var/log/shell_script/$0.log"
 
-mkdir -p $logs_folder
+
 if [ $userid -ne 0 ]; then
 echo "Please enter with root access"
 exit 1
 fi
 
+mkdir -p $logs_folder
 validate(){
     if [ $1 -ne 0 ];then
     echo "$2 is failed" | tee -a $logs_file
@@ -17,8 +18,8 @@ validate(){
     fi
 }
 
-dnf install nginx -y  &>>$logs_file
+dnf install nginx -y  &>> $logs_file
 validate $? "nginx installed" 
 
-dnf install mysql -y &>>$logs_file
+dnf install mysql -y &>> $logs_file
 validate $? "mysql installed"
