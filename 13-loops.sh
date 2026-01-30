38e4ec82a6e6f68991768a774daa1f555359c3a1
msaivenkatasiva <sivasl0597@gmail.com>
2026-01-30 21:48:30 +0530

    variables

--- 13-loops.sh
+++ 13-loops.sh
@@ -18,10 +18,10 @@ validate() {
 
 for package in $@
 do 
-dnf list $package installed &>> $log_file
-if [ $1 -ne 0 ];then
+dnf list installed $package &>> $log_file
+if [ $? -ne 0 ]; then
 echo "$package not installed ...installing now"
-dnf install $package -y &>> $log_file
+dnf install $package -y &>>$log_file
 validate $? "$package installation"
 else
 echo "$package already installed" &>> $log_file
