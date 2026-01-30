6dc04603991746139098e9aa8ffbebb1f158bcc5
msaivenkatasiva <sivasl0597@gmail.com>
2026-01-30 17:06:37 +0530

    variables

--- 09-installation.sh
+++ 09-installation.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 userid= $(id -u)
-if [$userid -ne 0]
+if [$userid -ne 0];
 then
 echo "Please enter with root acess"
-else
-dnf install inginx;
-if [$? -ne 0]
+exit 1
+fi
+echo "installing nginx"
+dnf install inginx -y
+if [$? -ne 0];
 then
 echo " installing nginx failuer"
+exit 1
 else
 echo "installed successfully"
 fi
