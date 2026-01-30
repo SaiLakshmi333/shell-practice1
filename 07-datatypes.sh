f21c68db0d9a1498b38811c2877b919150b8dcbd
msaivenkatasiva <sivasl0597@gmail.com>
2026-01-30 13:51:41 +0530

    variables

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -4,7 +4,7 @@ b=2
 sum=$(($a+$b))
 echo "total is : $sum"
 
-list=("apple","mango","grape")
+list=("apple" "mango" "grape")
 echo "the first fruit is : ${list[0]}"
 echo "end second fruit is : ${list[1]}"
-echo "third fruit is : ${list[2]}
+echo "third fruit is : ${list[@]}
