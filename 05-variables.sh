d4ed39f8f84530d94ccb35e1c269dae0fb0a12c9
msaivenkatasiva <sivasl0597@gmail.com>
2026-01-30 11:43:53 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 timestamp=$(date)
 echo "the script executed at : $timestamp"
-starttime= $(date+%s)
+starttime=$(date+%s)
 endtime=$(date+%s)
 sleep 10
-echo "total time executed : (($startime-$endtime)) 
+echo "total time executed : (($startime-$endtime))"
