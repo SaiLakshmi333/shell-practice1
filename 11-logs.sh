b3099b8a51db6fd5321ac15c8eca97b106273cff
msaivenkatasiva <sivasl0597@gmail.com>
2026-01-30 20:43:28 +0530

    variables

--- 11-logs.sh
+++ 11-logs.sh
@@ -7,11 +7,13 @@ echo "Please enter with root access"
 exit 1
 fi
 mkdir -p $log_folder
-validate(){
+validate() {
     if [ $1 -ne 0 ];then
     echo "$2 is failed" | tee -a $log_file
+    exit 1
     else
     echo "$2 is success" | tee -a $log_file
+    fi
 }
 
 dnf install nginx -y  &>> $log_file
