f8060ce390ab9ae5bad8cfd05f8c200b089fb6a4
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-06 21:25:45 +0530

    update changes

---
+++ 19-disk-usage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+user_id=$(id -u)
+log_dir="/var/log/shell_script"
+log_file="$log_dir/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m" 
+Message=""
+
+disk_usage=$(df -hT grep -v $Filesystem | awk '{print $6}' -f | cut -d "%" -f1)
+disk_threshold=3
+
+while IFS= read -r line
+do
+USAGE=$(echo $line| awk '{print $6}'|cut -d "%" -f1)
+PARTITION=$(echo $line | aws '{print $7}')
+if[ $USAGE -gt disk_threshold];then
+Message+="DISK is Full on $USAGE:$PARTITION" 
+fi
+done <<< $disk_usage
+
+echo $Message
