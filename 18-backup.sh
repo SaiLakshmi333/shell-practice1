c78fab24c4c4ba277eb10cd2a6fb90d4d05f41ec
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-06 14:07:23 +0530

    changes

--- 18-backup.sh
+++ 18-backup.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 user_id=$(id -u)
-log_folder="/var/log/shell_script"
-log_file="/var/log/shell_script/backup.log"
-
-mkdir -p $log_folder
+log_dir="/var/log/shell_script/"
+log_file="$log_dir/backup.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m" 
+source_dir=$1
+dest_dir=$2
 if [ $user_id -ne 0 ];then
-echo "please enter with root access"
+echo -e "$R please access with root user $N"
 exit 1
 fi
 
 usage(){
-    echo "USAGE::sudo backup <source_dir> <dest_dir> <days> default 14 days"
-    exit1
-    fi}
+ echo -e  "$R USAGE :: sudo backup <source_dir> <dest_dir> <days>[default 14 days]$N"
+}
 
-if [ $# -ne 2 ];then
-usage
+if [ $# -lt 2 ];then
+usage 
 fi
+
+if [ ! -d source_dir ];then
+echo "source directory not exist"
+exit 1
+fi
+
+if [ ! -d dest_dir ];then
+echo "destination directory"
+exit 1
+fi
+
+
