2435f5c49122b512a14f0630fbd701bbff3e1334
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-06 21:38:12 +0530

    update changes

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -16,9 +16,11 @@ while IFS= read -r line
 do
 USAGE=$(echo $line | awk '{print $6}'| cut -d "%" -f1)
 PARTITION=$(echo $line | awk '{print $7}')
+
 if[ "$USAGE" -ge "$disk_threshold" ];then
 Message+="DISK is Full on $USAGE:$PARTITION" 
 fi
+
 done <<< $disk_usage
 
 echo "$Message"
